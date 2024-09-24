32de71aefa245df7c9bfab07e08c2e6c1ecd8fcf
ashibullah <ashibullahbhai@gmail.com>
2024-09-25 02:01:40 +0600

    Trying to make the calculation using function and made it Alhamdulillah

--- js/action.js
+++ js/action.js
@@ -1,15 +1,28 @@
 const balance = getInputInnerText('my-balance');
-const donatedOne = getInputInnerText('donated-one');
-
-function calculate(bal, donatedBal) {
+const donatedOne = getInputInnerText('donation-1');
 
 
-}
 
-document.getElementById('donate-btn-1').addEventListener('click', function (event) {
-    const donateAmount = getInputValue('donate-amount-1');
-    if (donateAmount === NaN) { alert("Enter Valid Amount"); }
-    else {
-        console.log(donateAmount)
-    }
-})
+// document.getElementById('donate-btn-1').addEventListener('click', calculation('donate-amount-1', 'donation-1', 'donation-title-1' ) )
+
+
+
+// document.getElementById('donate-btn-1').addEventListener('click', function () {
+//     const donateAmount = getInputValue('donate-amount-1');
+//     const balance = getInputInnerText('my-balance');
+//     const donatedOne = getInputInnerText('donation-1');
+//     if (isNaN(donateAmount) || donateAmount > balance || donateAmount <= 0) { alert("Enter Valid Amount"); }
+//     else {
+//         // console.log(donateAmount, banalce);
+//         const currentDonatedAmmount = getInputInnerText('donation-1');
+//         document.getElementById('donation-1').innerText = donateAmount + currentDonatedAmmount;
+//         document.getElementById('my-balance').innerText = (balance - donateAmount);
+//         const title = document.getElementById('donation-title-1').innerText;
+//         // console.log(title);
+//         addHistory(donateAmount, title );
+    
+//     }
+// })
+
+// document.getElementById('donation-toggle-btn').addEventListener('click', toggleBtn('donation-toggle'));
+// document.getElementById('history-toggle-btn').addEventListener('click', toggleBtn('history-toggle'));
--- js/function.js
+++ js/function.js
@@ -1,3 +1,22 @@
+function calculation(amountOfDonate, amountAlreadyDonated, donationTitle){
+    console.log("Running Calculation function");
+    const donateAmount = getInputValue(amountOfDonate);
+    const balance = getInputInnerText('my-balance');
+    const donatedOne = getInputInnerText(amountAlreadyDonated);
+    if (isNaN(donateAmount) || donateAmount > balance || donateAmount <= 0) { alert("Enter Valid Amount"); }
+    else {
+        // console.log(donateAmount, banalce);
+        const currentDonatedAmmount = getInputInnerText(amountAlreadyDonated);
+        document.getElementById(amountAlreadyDonated).innerText = donateAmount + currentDonatedAmmount;
+        document.getElementById('my-balance').innerText = (balance - donateAmount);
+        const title = document.getElementById(donationTitle).innerText;
+        // console.log(title);
+        addHistory(donateAmount, title );
+    
+    }
+
+}
+
 function getInputInnerText(id){
     const inputValue = parseFloat(document.getElementById(id).innerText);
     return inputValue;
@@ -6,3 +25,23 @@ function getInputValue(id){
     const inputValue = parseFloat(document.getElementById(id).value);
     return inputValue;
 }
+
+function toggleBtn(id){
+    console.log();
+    document.getElementById('history-toggle').classList.add('hidden');
+    document.getElementById('donation-toggle').classList.add('hidden');
+
+    document.getElementById(id).classList.remove('hidden');
+}
+
+function addHistory(amount,title){
+    const time = new Date();
+    const div = document.createElement('div');
+    div.classList.add('mt-8', 'p-6' , 'border' , 'rounded-lg' , 'lg:max-w-6xl');
+    div.innerHTML = `
+    <h1 class="text-lg font-semibold mb-2">You Donated ${amount} BDT to ${title} </h1>
+    <p>Time: ${time.getHours()}:${time.getMinutes()} on ${time.getDate()}/${time.getMonth() + 1}/${time.getFullYear()}</p>
+    `
+    document.getElementById('history-toggle').appendChild(div);
+}
+
